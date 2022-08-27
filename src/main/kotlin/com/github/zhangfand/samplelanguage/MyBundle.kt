89994748749676f820ed7a61c8c6d727e7380a36
GitHub Action <action@github.com>
2022-08-27 04:39:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhangfand/samplelanguage/MyBundle.kt
+++ src/main/kotlin/com/github/zhangfand/samplelanguage/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhangfand.samplelanguage
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhangfand/samplelanguage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhangfand/samplelanguage/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhangfand.samplelanguage.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhangfand.samplelanguage.MyBundle
 
 class MyProjectService(project: Project) {
 
